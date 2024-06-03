a3aeb159967ccaad579620cf955727cf9d638a2a
Pierre-Henry Soria <pierre@ph7.me>
2024-06-04 07:53:42 +1000

    Add databaseUrl to env

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,15 @@ dotenv.config();
 interface ConfigProps {
   port: string | number;
   host: string;
+  databaseUrl: string;
 }
 
 const config: ConfigProps = {
   port: process.env.PORT || 3000,
   host: process.env.HOST || "localhost",
+  databaseUrl:
+    process.env.DATABASE_URL ||
+    "mysql://root:root@localhost:3306/habit_tracker",
 };
 
 export { config };
