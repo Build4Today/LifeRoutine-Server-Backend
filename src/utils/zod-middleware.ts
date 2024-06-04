6d0bdd1da8a7d9c48006cc436df7e40bceaf86df
Pierre-Henry Soria <pierre@ph7.me>
2024-06-04 20:30:06 +1000

    Add comments for improving clearity/understanding

--- src/utils/zod-middleware.ts
+++ src/utils/zod-middleware.ts
@@ -4,7 +4,10 @@ import { AnyZodObject, ZodError } from "zod";
 export function createZodMiddleware(schema: AnyZodObject) {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     try {
+      // First, attempt to parse the request body
       const data = await schema.parseAsync(request.body);
+
+      // Assign the parsed data to the request body
       request.body = data;
     } catch (error) {
       if (error instanceof ZodError) {
