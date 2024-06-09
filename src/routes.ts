c4664cd188d7086ee290a114cb6b46f82db175c2
Pierre-Henry Soria <pierre@ph7.me>
2024-06-10 00:10:26 +1000

    add `logger.error` in catch{} block for easier debugging

--- src/routes.ts
+++ src/routes.ts
@@ -258,6 +258,8 @@ export async function appRoutes(app: FastifyInstance) {
           });
         }
       } catch (error) {
+        logger.error(error);
+
         reply
           .status(StatusCodes.INTERNAL_SERVER_ERROR)
           .send({ error: "Internal Server Error" });
