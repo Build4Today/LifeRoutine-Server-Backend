f716621250a3976e0b19967060e28757859bac56
Pierre-Henry Soria <pierre@ph7.me>
2024-06-09 17:05:52 +1000

    Add loggin in catch {} block

--- src/routes.ts
+++ src/routes.ts
@@ -182,6 +182,8 @@ export async function appRoutes(app: FastifyInstance) {
           completedHabits,
         };
       } catch (error) {
+        logger.error(error);
+
         reply
           .status(StatusCodes.INTERNAL_SERVER_ERROR)
           .send({ error: "Internal Server Error" });
